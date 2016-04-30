290947174070293dc16cbd6a65f82c998f007127
giantmolecules <giantmolecules@gmail.com>
2016-04-30 15:22:58 -0500

    Added files via upload

---
+++ access.php
@@ -0,0 +1,16 @@
+<?php
+//
+// ACCESS.PHP
+// Brett Ian Balogh
+// April 30, 2016
+// brettbalogh@gmail.com
+//
+// $ACCESS_TOKEN = "YOUR_ACCESS_TOKEN"
+//
+$ACCESS_TOKEN = "";
+//
+// $DEVICE_ID = array("DEVICE_ID_1", "DEVICE_ID_2", etc.)
+//
+$DEVICE_ID = array("");
+//
+?>
